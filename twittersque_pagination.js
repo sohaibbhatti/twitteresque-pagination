1aebb906d98111cf052d733b142fb82044ce730f
Sohaib Bhatti <sohaib.bhatti@7vals.com>
2012-03-07 22:42:13 +0500

    added argument for container as well

--- twittersque_pagination.js
+++ twittersque_pagination.js
@@ -1,34 +1,36 @@
-function triggerPagination(pageUrl, pageNum, maxPages, threshold, timeoutValue){
+function triggerPagination(pageUrl, pageNum, maxPages, threshold, timeoutValue, container){
+  container = (typeof container == 'undefined')? '.results' : container;
   nextPage = pageNum + 1;
-  if (triggerComparator(threshold)){
+  if (triggerComparator(threshold, container)){
     if (pageNum<=maxPages){
       pageRequest = pageUrl.concat('.js');
       $.get(pageRequest, {page: pageNum});
     }
     if (nextPage<=maxPages){
       $(document).ajaxComplete(function(){
-        triggerPagination(pageUrl, nextPage, maxPages, threshold, timeoutValue)
+        triggerPagination(pageUrl, nextPage, maxPages, threshold, timeoutValue, container);
       });
     }
   }
   else{
     window.setTimeout(
       function(){
-        triggerPagination(pageUrl, pageNum, maxPages, threshold, timeoutValue);
+        triggerPagination(pageUrl, pageNum, maxPages, threshold, timeoutValue, container);
       },
       300
     );
   } 
 }
 
-function triggerComparator(threshold){
-  return (calculateScrollerPosition() < threshold);
+function triggerComparator(threshold, container){
+  return (calculateScrollerPosition(container) < threshold);
 }
 
-function calculateScrollerPosition(){
-  return (divHeight() - ($(document).scrollTop() + $(window).height()));
+function calculateScrollerPosition(container){
+  return (divHeight(container) - ($(document).scrollTop() + $(window).height()));
 }
 
-function divHeight(){
-  return ($('.results').position().top + $('.results').height());
+function divHeight(container){
+  return ($(container).position().top + $(container).height());
 }
+
